d0205da56f4e7c4237a7a7709eb1551fb4ad5529
PetharaGuruge <135795367+PetharaGuruge@users.noreply.github.com>
2023-07-31 20:30:48 +0530

    Lab 07

---
+++ Tutorial 07/Q1.php
@@ -0,0 +1,9 @@
+<?php  
+  $x=6;  
+  $y=4;  
+  $z=;  
+  echo "Sum: ".($x+$y)."<br>";  
+  echo "Difference: ".($x-$y)."<br>";
+  echo "Product: ".($x*$y)."<br>";
+  echo "Division: ".($x/$y)."<br>";
+?>
